65cabd43ef08dcbb6859e948ab3a5c81defc7af1
Parker <crookparker99@gmail.com>
2025-04-18 12:04:43 -0400

    Create ability to add notes and redirect to home page

---
+++ README.md
@@ -0,0 +1,21 @@
+📌 Phase 1: Basic Note Taking (No DB)
+ Set up Flask and routes
+
+ - Build a form to add a note (title + content)
+ - Store notes in a Python list (temporarily, in memory)
+
+📌 Phase 2: Add Templates
+
+ - Use Jinja2 to render a notes list
+ - Use Bootstrap for basic styling
+
+📌 Phase 3: Database Integration
+
+ - Use SQLite + SQLAlchemy
+ - Create a Note model (id, title, content)
+ - Store and retrieve notes from DB
+
+📌 Phase 4 (Optional): Authentication
+
+ - Add login/register with password hashing
+ - Each user sees only their notes
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, redirect, url_for, render_template
+
+app = Flask(__name__)
+notes = []
+
+@app.route("/")
+def home():
+    return render_template("home.html", notes=notes)
+
+@app.route("/add", methods=['GET', 'POST'])
+def add_note():
+    if request.method == "GET":
+        return render_template("note_form.html")
+    elif request.method == "POST":
+        note = {}
+        note_title = request.form.get("title")
+        note_content = request.form.get("content")
+        note["title"] = note_title
+        note["content"] = note_content
+        notes.append(note)
+        return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
